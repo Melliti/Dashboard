21f45ca2fd8629ff1ad031662049a2279f4980db
melliti <ramzi.melliti@epitech.eu>
2019-03-15 00:20:44 +0100

    [ADD] front submit footballwidget

--- front/src/widgets/FootballWidget.js
+++ front/src/widgets/FootballWidget.js
@@ -31,27 +31,42 @@ class FootballWidget extends Component {
         .catch(err => console.error(err))
     }
 
-    renderContent = ({ id, crestUrl, shortName }) => <div key={id} className="clubInfo"><img src={crestUrl} className="clubLogo"/>{shortName}</div>
+    renderContent = ({ id, crestUrl, shortName }) => <div key={id} className="clubInfo"><img src={crestUrl} alt="logo" className="clubLogo"/>{shortName}</div>
 
     render() {
-        const { teams, league } = this.state;
+        const { teams } = this.state;
         return (
-            <div className="col-md-4">
-                { teams.map(this.renderContent) }
-                    <div className="form-group col-md-8">
-                        <label>Choose a league</label>
-                        <select 
-                            className="form-control"
-                            id="selector" 
-                            onChange={e => this.setState({ league: {...teams, league: e.target.value}})}>
-                            <option value="FL1">Ligue 1</option>
-                            <option value="PL">Premier League</option>
-                            <option value="BL1">BundesLiga</option>
-                            <option value="SA">Serie A</option>
-                            <option value="PD">Liga</option>
-                        </select>
-                    </div>
-                    <button className="btn btn-primary" onClick={e => this.setLeague(e.target.value)}>Submit</button>
+            <div className="col-md-4 widget">
+                <div className="clubList">
+                    { teams.map(this.renderContent) }
+                </div>
+                <div 
+                    className="form-group col-md-12" 
+                    style={{paddingLeft: 0, marginTop: 5, marginBottom: 5}}>
+                    <label 
+                        htmlFor="league" 
+                        className="col-sm-5 pull-right" 
+                        style={{padding: 0}} >Choose a league
+                    </label>
+                    <select
+                        style={{padding: 0}}
+                        className="col-sm-7 pull-right"
+                        id="selector" 
+                        onChange={e => this.setState({ league: {...teams, league: e.target.value}})}>
+                        <option value="FL1">Ligue 1</option>
+                        <option value="PL">Premier League</option>
+                        <option value="BL1">BundesLiga</option>
+                        <option value="SA">Serie A</option>
+                        <option value="PD">Liga</option>
+                    </select>
+                    <button 
+                    style={{padding: 0, borderRadius: 0, verticalAlign: 0}} 
+                    className="btn btn-secondary col pull-right" 
+                    onClick={e => this.setLeague(e.target.value)}>
+                        Submit
+                    </button>    
+                </div>
+                
             </div>
         )
     }
